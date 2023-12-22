82376e770054b938b1cbc2fbb7ac41fb78839673
Reyhan Anderea Firdaus <reihan676590@gmail.com>
2023-12-22 15:29:01 +0700

    melanjutkan fitur search

--- search/views.py
+++ search/views.py
@@ -3,13 +3,19 @@ from users.views import get_data_user_person
 
 # Create your views here.
 def index(request):
-  getting_users = get_data_user_person()
   users_data = []
-  for i in getting_users:
-    fetch = {
-      'nis': i['nis'],
-      'nama_lengkap': i['nama_lengkap'],
-    }
-    users_data.append(fetch)
+  message = ""
+  if request.method == "POST":
+    search_user = request.POST["search_user"]
+    getting_users = get_data_user_person(search_user)
+    for i in getting_users:
+      fetch = {
+        'nis': i['nis'],
+        'nama_lengkap': i['nama_lengkap'],
+      }
+      users_data.append(fetch)
+  else:
+    message = "Ayo Cari Temanmu"
     
-  return render(request, "search/search.html", {'content':users_data})
+  print(users_data)
+  return render(request, "search/search.html", {'content':users_data, "message":message})
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from . import models
 from logreg import forms as lrforms
 from django.contrib.auth.models import User
 from .models import Person
+from django.db.models import Q
 
 # Create your views here.
 def users(request):
@@ -13,7 +14,6 @@ def users(request):
 def profil_btn(request):
   if request.user:
     user = request.user
-    print(user)
     return redirect('../../' + user.username + '/')
   else:
     return redirect("welcome")
@@ -62,24 +62,33 @@ def add_profile(request):
   return render(request, 'person/edit_profil.html', {'form': form,'formAuth':formAuth, 'username': username, 'succes':""})
 
 
-def get_data_user_person():
+def get_data_user_person(filters="all"):
   userperson = [
       ]
-  person_data = Person.objects.values_list("nis", "kelas", "jurusan", "jenis_kelamin", "hobi")
+  if filters == "all":
+    users_data = User.objects.all()
+  elif filters != "all":
+    if User.objects.filter(Q(username__icontains=filters) | Q(first_name__icontains=filters)).exists():
+        users_data = User.objects.filter(
+          Q(username__icontains=filters) | Q(first_name__icontains=filters)
+          )
+    else:
+        users_data = User.objects.all()
     
-  for i in range(len(person_data)):
-      basicinfo = User.objects.get(username=person_data[i][0])
-      otherinfo = person_data[i]
-      data_profile  = {
-        "nis": basicinfo.username,
-        "nama_lengkap": basicinfo.first_name,
-        "nama_panggilan": basicinfo.last_name,
-        "kelas" : otherinfo[1],
-        "jurusan" : otherinfo[2],
-        "jenis_kelamin" : otherinfo[3],
-        "hobi" : otherinfo[4],
-      }
-      
-      userperson.append(data_profile)
+  
+  for user in users_data:
+    print(user)
+    person_info = Person.objects.get(nis=user.username)
+    data_profile  = {
+      "nis": user.username,
+      "nama_lengkap": user.first_name,
+      "nama_panggilan": user.last_name,
+      "kelas" : person_info.kelas,
+      "jurusan" : person_info.jurusan,
+      "jenis_kelamin" : person_info.jenis_kelamin,
+      "hobi" : person_info.hobi,
+    }
+    userperson.append(data_profile)
+  
   return userperson
 
