bca741fee18d4e9d5b69fa1a1c5e18d6fa4744cb
Reyhan Anderea Firdaus <reihan676590@gmail.com>
2023-12-25 08:55:14 +0700

    menambahkan fungsi tombol view

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,36 +1,36 @@
-tippy('.link',{
-		  placement: 'bottom'
-		})
+// tippy('.link',{
+// 		  placement: 'bottom'
+// 		})
 
-		//Toggle mode
-		const toggle = document.querySelector('.js-change-theme');
-		const body = document.querySelector('body');
-		const profile = document.getElementById('profile');
-		const moon = document.getElementById('moon');
-		const sun = document.getElementById('sun');
-		const svgel = document.getElementById("svgc");
+// 		//Toggle mode
+// 		const toggle = document.querySelector('.js-change-theme');
+// 		const body = document.querySelector('body');
+// 		const profile = document.getElementById('profile');
+// 		const moon = document.getElementById('moon');
+// 		const sun = document.getElementById('sun');
+// 		const svgel = document.getElementById("svgc");
 		
-		toggle.addEventListener('click', () => {
+// 		toggle.addEventListener('click', () => {
 
-		  if (body.classList.contains('text-indigo-400')) {
-			// to light
-			sun.classList.add("hidden");
-			moon.classList.remove("hidden")
-			body.classList.remove('text-indigo-400','tdark');
-			body.classList.add('text-indigo-900','tlight');
-			svgel.style.fill = 'black';
-			profile.classList.remove('bg-gray-900');
-			profile.classList.add('bg-white');
-		  } else
-		  {
-		  // to dark
-			moon.classList.add("hidden");
-			sun.classList.remove("hidden")
-			body.classList.remove('text-indigo-900','tlight');
-			body.classList.add('text-indigo-400','tdark');
-			svgel.style.fill = 'white';
-			profile.classList.remove('bg-white');			
-			profile.classList.add('bg-gray-900');
+// 		  if (body.classList.contains('text-indigo-400')) {
+// 			// to light
+// 			sun.classList.add("hidden");
+// 			moon.classList.remove("hidden")
+// 			body.classList.remove('text-indigo-400','tdark');
+// 			body.classList.add('text-indigo-900','tlight');
+// 			svgel.style.fill = 'black';
+// 			profile.classList.remove('bg-gray-900');
+// 			profile.classList.add('bg-white');
+// 		  } else
+// 		  {
+// 		  // to dark
+// 			moon.classList.add("hidden");
+// 			sun.classList.remove("hidden")
+// 			body.classList.remove('text-indigo-900','tlight');
+// 			body.classList.add('text-indigo-400','tdark');
+// 			svgel.style.fill = 'white';
+// 			profile.classList.remove('bg-white');			
+// 			profile.classList.add('bg-gray-900');
 			
-		  }
-		});
+// 		  }
+// 		});
